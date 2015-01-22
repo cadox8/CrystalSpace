6817a5f693ebe0fdad8ec231cce6ef0a84e7899d
Justin W. Flory <me@justinwflory.com>
2015-01-22 08:51:47 -0500

    Remove Spout support for player interaction in black holes

--- src/main/java/com/crystalcraftmc/crystalspace/listeners/misc/BlackHolePlayerListener.java
+++ src/main/java/com/crystalcraftmc/crystalspace/listeners/misc/BlackHolePlayerListener.java
@@ -7,7 +7,6 @@ import com.crystalcraftmc.crystalspace.handlers.ConfigHandler;
 import com.crystalcraftmc.crystalspace.handlers.SpoutHandler;
 import com.crystalcraftmc.crystalspace.handlers.WorldHandler;
 import com.crystalcraftmc.crystalspace.runnables.SpoutBlackHoleChaosRunnable;
-import com.crystalcraftmc.crystalspace.wgen.blocks.BlackHole;
 import com.crystalcraftmc.crystalspace.wgen.populators.SpaceBlackHolePopulator;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
@@ -20,30 +19,26 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.getspout.spoutapi.block.SpoutBlock;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-// bSpace Imports
-// Bukkit Imports
-// Spout Imports
 
 /**
  * Player listener to trigger black hole sucking.
  *
  * @author iffamies
+ * @author jflory7
  */
 public class BlackHolePlayerListener implements Listener {
     // Variables
     private static Map<Player, Integer> runnables = new HashMap<Player, Integer>();
-    private static Map<Chunk, Boolean> scannedSpout = new HashMap<Chunk, Boolean>();
     private static Map<Chunk, Boolean> scannedNonSpout = new HashMap<Chunk, Boolean>();
     private static final int SIZE = 20;
     private static long lastTime = System.currentTimeMillis();
-    private static List<Block> nonSpoutBlocks = new ArrayList<Block>();//To be used if not using spout and blackholes is on
+    private static List<Block> nonSpoutBlocks = new ArrayList<Block>();//To be used if not using spout and black holes is on
 
     /**
      * Called when a player attempts to move.
@@ -52,40 +47,26 @@ public class BlackHolePlayerListener implements Listener {
      */
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerMove(PlayerMoveEvent event) {
-        if (event.isCancelled() || !WorldHandler.isInAnySpace(event.getPlayer()) || event.getPlayer().getHealth() == 0 || event.getPlayer().hasPermission("bSpace.ignoreblackholes")) {
-            return;
-        }
+        if (event.isCancelled() || !WorldHandler.isInAnySpace(event.getPlayer()) || event.getPlayer().getHealth() == 0 || event.getPlayer().hasPermission("bSpace.ignoreblackholes")) return;
         long currentTime = System.currentTimeMillis();
-        if (!(lastTime + 200 <= currentTime)) {
-            return;
-        }
+        if (!(lastTime + 200 <= currentTime)) return;
         lastTime = System.currentTimeMillis();
-	if(ConfigHandler.getGenerateBlackHolesSpout(ConfigHandler.getID(event.getTo().getWorld()))){
-	    checkBlocksSpout(event.getTo());
-	    for (SpoutBlock block : BlackHole.getHolesList()) {
-		if (SpoutHandler.isInsideRadius(event.getPlayer(), block.getLocation(), SIZE) && !runnables.containsKey(event.getPlayer())) {
-		    int taskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(Bukkit.getPluginManager().getPlugin("bSpace"), new SpoutBlackHoleChaosRunnable(event.getPlayer(), block), 0, (long) 1); //Period
-		    runnables.put(event.getPlayer(), taskId);
-		    event.getPlayer().sendMessage("Black hole. -bam-, you're dead.");
-		    return;
-		}
+
+	    if(ConfigHandler.getGenerateBlackHolesNonSpout(ConfigHandler.getID(event.getTo().getWorld()))){
+	        checkBlocksNonSpout(event.getTo());
+	        for (Block block : nonSpoutBlocks) {
+		        if (SpoutHandler.isInsideRadius(event.getPlayer(), block.getLocation(), SIZE) && !runnables.containsKey(event.getPlayer())) {
+                    int taskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(Bukkit.getPluginManager().getPlugin("bSpace"), new SpoutBlackHoleChaosRunnable(event.getPlayer(), block), 0, (long) 1); //Period
+                    runnables.put(event.getPlayer(), taskId);
+                    event.getPlayer().sendMessage("Black hole. -bam-, you're dead.");
+                    return;
+		        }
+	        }
 	    }
-	}
-	else if(ConfigHandler.getGenerateBlackHolesNonSpout(ConfigHandler.getID(event.getTo().getWorld()))){
-	    checkBlocksNonSpout(event.getTo());
-	    for (Block block : nonSpoutBlocks) {
-		if (SpoutHandler.isInsideRadius(event.getPlayer(), block.getLocation(), SIZE) && !runnables.containsKey(event.getPlayer())) {
-		    int taskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(Bukkit.getPluginManager().getPlugin("bSpace"), new SpoutBlackHoleChaosRunnable(event.getPlayer(), block), 0, (long) 1); //Period
-		    runnables.put(event.getPlayer(), taskId);
-		    event.getPlayer().sendMessage("Black hole. -bam-, you're dead.");
-		    return;
-		}
-	    }
-	}
     }
     
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onPlayerInteract(PlayerInteractEvent event){// Check if breaking non-spout blackhole
+    public void onPlayerInteract(PlayerInteractEvent event){// Check if breaking non-spout black hole
 	if (event.isCancelled() || !WorldHandler.isInAnySpace(event.getPlayer()) || event.getPlayer().getHealth() == 0 || !event.getPlayer().hasPermission("bSpace.ignoreblackholes")) {
             return;
         }
@@ -111,31 +92,6 @@ public class BlackHolePlayerListener implements Listener {
         Bukkit.getScheduler().cancelTask(runnables.get(player));
         runnables.remove(player);
     }
-
-    private static void checkBlocksSpout(Location loc) {
-        Chunk center = loc.getChunk();
-        int chunks = (int) Math.ceil((SIZE - 1) / 16);
-        chunks = chunks > Bukkit.getViewDistance() ? Bukkit.getViewDistance() : chunks;
-        for (int chunkX = -chunks; chunkX <= chunks; chunkX++) {
-            for (int chunkZ = -chunks; chunkZ <= chunks; chunkZ++) {
-                Chunk chunk = loc.getWorld().getChunkAt(center.getX() + chunkX, center.getZ() + chunkZ);
-                if (scannedSpout.containsKey(chunk) && scannedSpout.get(chunk) == true) {
-                    continue;
-                }
-                for (int x = 0; x < 16; x++) {
-                    for (int y = 0; y < 128; y++) {
-                        for (int z = 0; z < 16; z++) {
-                            SpoutBlock block = (SpoutBlock) chunk.getBlock(x, y, z);
-                            if (block.getBlockType() instanceof BlackHole && !BlackHole.getHolesList().contains(block)) {
-                                BlackHole.getHolesList().add(block);
-                            }
-                        }
-                    }
-                }
-                scannedSpout.put(chunk, true);
-            }
-        }
-    }
     
     private static void checkBlocksNonSpout(Location loc) {
         Chunk center = loc.getChunk();
@@ -144,7 +100,7 @@ public class BlackHolePlayerListener implements Listener {
         for (int chunkX = -chunks; chunkX <= chunks; chunkX++) {
             for (int chunkZ = -chunks; chunkZ <= chunks; chunkZ++) {
                 Chunk chunk = loc.getWorld().getChunkAt(center.getX() + chunkX, center.getZ() + chunkZ);
-                if (scannedNonSpout.containsKey(chunk) && scannedNonSpout.get(chunk) == true) {
+                if (scannedNonSpout.containsKey(chunk) && scannedNonSpout.get(chunk)) {
                     continue;
                 }
                 for (int x = 0; x < 16; x++) {
